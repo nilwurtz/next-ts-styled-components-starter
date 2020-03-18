99395f41738507d113a966a8a58f49f26645cdce
ragnar <ragnar@ragnarPC.local>
2020-03-18 21:22:39 +0900

    upgrade

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width,height=device-height" key="viewport" />
           <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap" rel="stylesheet"></link>
+          {this.props.styles}
         </Head>
 
         <body>
